907fb830b72ec8c0fa9363b62b02147be337c9fb
Federico <federico.izzop@gmail.com>
2021-05-31 17:39:57 +0200

    fix

--- src/components/Header.js
+++
@@ -1,5 +0,0 @@
-export default function Header({ section }) {
-  return (
-    <div>{section.title}</div>
-  );
-}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,11 @@
 import '../scss/style.scss';
-import Header from './Header';
 import Seo from './Seo';
 
 export default function Layout({ children, section }) {
   return (
     <div>
       <Seo section={section} />
-      <Header section={section} />
+
       {children}
 
     </div>
