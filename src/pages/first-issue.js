cf4fa363a723add1ee59e76b57f2ed498611caf1
Federico <federico.izzop@gmail.com>
2021-10-12 14:51:31 +0200

    first issue

--- src/pages/first-issue.js
+++ src/pages/first-issue.js
@@ -8,12 +8,13 @@ import img2 from '../images/first-issue/2-intro.png';
 import img3 from '../images/first-issue/3-bolsVsMacr.png';
 import img4 from '../images/first-issue/4-rightVsLeft.png';
 import img5 from '../images/first-issue/5-proAgainst@2x.png';
-import img6 from '../images/first-issue/6-forabolsonaro.png';
+
 import img61 from '../images/first-issue/6-forabols.png';
+import img6 from '../images/first-issue/prova.png';
 import img62 from '../images/first-issue/6-focus1.png';
 import img63 from '../images/first-issue/6-focus2.png';
 import img64 from '../images/first-issue/6-panelaco.png';
-import img7 from '../images/first-issue/7-macronliarFull.png';
+import img7 from '../images/first-issue/7-prova.png';
 import img71 from '../images/first-issue/7-macronliarTweet.png';
 import img72 from '../images/first-issue/7-focus1.png';
 import img73 from '../images/first-issue/7-vivabolsonaroTweet.png';
@@ -22,6 +23,8 @@ import img8 from '../images/first-issue/8-deforestationDrivers.png';
 import img9 from '../images/first-issue/9-linking@2x.png';
 import img91 from '../images/first-issue/9-focus1.png';
 import img92 from '../images/first-issue/9-focus2.png';
+import img10 from '../images/first-issue/10-all.png';
+import img11 from '../images/first-issue/11-full@2x.png';
 import Image from '../components/Image';
 import NextPrev from '../components/NextPrev';
 
@@ -140,64 +143,67 @@ export default function firstIssue() {
         </div>
 
         <div className="row py-5">
-          <div className="col-5 offset-1">
-            <Heading3 classi="pb-3" text="Left-leaning voices against Bolsonaro are clustered and more prominent" />
-            <p>
-              When zooming into
-              {' '}
-              <span className="text-warning">#forabolsonaro</span>
-              , one of the most popular hashtags against Bolsonaro with over 2300 tweets, the top tweet was a tweet by Dilma Bolada, a fictional character who became a social media influencer  during  the terms of Dilma Rousseff, the former Brazilian president.. With 1.1 million followers, the account called for protests  in various cities in Brazil. Another tweet posted a video claiming to show the fires reaching a residential street in the state of Para, one of the Brazilian states with  an increasing deforestation rate. There was also a tweet with a video of protests at the Brazilian embassy in Peru, using hashtags that criticise Bolsonaro and Salles, the former Brazilian Environment Minister.
-            </p>
-            <Image src={img6} />
-            <p>
-              Dilma Bolada’s account was also visible when looking into the hashtags
-              {' '}
-              <span className="text-warning">#panelaco</span>
-              {' '}
-              and
-              {' '}
-              <span className="text-warning">#panelaço</span>
-              . Panelaco is a form of protest which was originally used against the former Dilma government and was later taken over by the protestors against Bolsonaro. The social media influencer’s account tweeted calling for a balcony protest by banging pots and pans at their window using a photo of the former president Dilma holding a pan and sharing a video of panelaço in Rio de Janeiro. One of these tweets was also shared by the Brazilian Senator Humberto Costa and the former Health Minister.
-            </p>
-          </div>
-          <div className="col-3 offset-2">
-            <Image classi="pb-3" src={img61} />
-            <Image classi="py-3" src={img62} />
-            <Image classi="py-3" src={img63} />
-            <Image classi="pt-3" src={img64} />
+          <div className="row">
+            <div className="col-5 offset-1">
+              <Heading3 classi="pb-3" text="Left-leaning voices against Bolsonaro are clustered and more prominent" />
+              <p>
+                When zooming into
+                {' '}
+                <span className="text-warning">#forabolsonaro</span>
+                , one of the most popular hashtags against Bolsonaro with over 2300 tweets, the top tweet was a tweet by Dilma Bolada, a fictional character who became a social media influencer  during  the terms of Dilma Rousseff, the former Brazilian president.. With 1.1 million followers, the account called for protests  in various cities in Brazil. Another tweet posted a video claiming to show the fires reaching a residential street in the state of Para, one of the Brazilian states with  an increasing deforestation rate. There was also a tweet with a video of protests at the Brazilian embassy in Peru, using hashtags that criticise Bolsonaro and Salles, the former Brazilian Environment Minister.
+              </p>
+            </div>
+            <div className="col-3 offset-2">
+              <Image classi="" src={img61} />
+            </div>
+            <div className="col-10 offset-1">
+              <Image classi="pb-5" src={img6} />
+            </div>
+            <div className="col-5 offset-1">
+              <p>
+                Dilma Bolada’s account was also visible when looking into the hashtags
+                {' '}
+                <span className="text-warning">#panelaco</span>
+                {' '}
+                and
+                {' '}
+                <span className="text-warning">#panelaço</span>
+                . Panelaco is a form of protest which was originally used against the former Dilma government and was later taken over by the protestors against Bolsonaro. The social media influencer’s account tweeted calling for a balcony protest by banging pots and pans at their window using a photo of the former president Dilma holding a pan and sharing a video of panelaço in Rio de Janeiro. One of these tweets was also shared by the Brazilian Senator Humberto Costa and the former Health Minister.
+              </p>
+            </div>
+            <div className="col-3 offset-2 mt-n5">
+              <Image classi=" " src={img64} />
+            </div>
           </div>
         </div>
 
-        <div className="row py-4">
-          <div className="col-5 offset-1">
-            <Heading3 classi="pb-3" text="While not as prominent as those against him, voices supporting Bolsonaro were also present" />
-            <p>
-              When zooming into pro-Bolsonaro hashtags it is possible to identify tweets from pro-Bolsonaro accounts, many of which are suspended on Twitter. For instance,
-              <span className="text-secondary"> #MacronLiar</span>
-              {' '}
-              was used to criticise the NGOs presence in the Amazon. The narrative corresponds to Bolsonaro’s statement on 21 August 2019 accusing them of setting the fires. .
-            </p>
-            <div className="row">
-              <div className="col-8">
-                <Image classi="py-3" src={img71} />
-                <Image classi="py-3" src={img72} />
-              </div>
+        <div className="row py-5">
+          <div className="row mb-n5">
+            <div className="col-5 offset-1">
+              <Heading3 classi="pb-3" text="While not as prominent as those against him, voices supporting Bolsonaro were also present" />
+              <p>
+                When zooming into pro-Bolsonaro hashtags it is possible to identify tweets from pro-Bolsonaro accounts, many of which are suspended on Twitter. For instance,
+                <span className="text-secondary"> #MacronLiar</span>
+                {' '}
+                was used to criticise the NGOs presence in the Amazon. The narrative corresponds to Bolsonaro’s statement on 21 August 2019 accusing them of setting the fires. .
+              </p>
             </div>
-            <p className="py-3">
-              Another pro-Bolsonaro hashtag,
-              {' '}
-              <span className="text-secondary">  #vivabolsonaro </span>
-              , was used in a tweet by a columnist of Breitbart, an American far-right news site. The tweet included a link to the Breitbart’s article “Delingpole: Amazon Fires – A Big, Fat Nothingburger of a #FakeNews Scare Story”. The article criticised the news from the mainstream media as “fake news” and twisted the narrative to portray alternative ways to interpret the scientific insights. Interestingly, to do so, the article used a mix of references ranging from tweets from suspended accounts to those from scientists.
-            </p>
+            <div className="col-3 offset-2">
+              <Image classi="py-3" src={img71} />
+            </div>
           </div>
-          <div className="col-5 ">
-            <div className="row">
-              <div className="col-8 offset-4">
-                <Image classi="py-3" src={img73} />
-                <Image classi="py-3" src={img74} />
-              </div>
+          <div className="row my-n5">
+            <div className="col-10 offset-1">
+              <Image classi="py-3" src={img7} />
             </div>
-            <Image classi="py-3" src={img7} />
+            <div className="col-5 offset-1 mt-n5">
+              <p className="py-3">
+                Another pro-Bolsonaro hashtag,
+                {' '}
+                <span className="text-secondary">  #vivabolsonaro </span>
+                , was used in a tweet by a columnist of Breitbart, an American far-right news site. The tweet included a link to the Breitbart’s article “Delingpole: Amazon Fires – A Big, Fat Nothingburger of a #FakeNews Scare Story”. The article criticised the news from the mainstream media as “fake news” and twisted the narrative to portray alternative ways to interpret the scientific insights. Interestingly, to do so, the article used a mix of references ranging from tweets from suspended accounts to those from scientists.
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -238,7 +244,7 @@ export default function firstIssue() {
               {' '}
               <span className="text-secondary"> There is also a cluster on the top left where #taxpayer, #bank and #cocoa sit close to #trumpaskidsincage and #liarinchief.</span>
             </p>
-            <Image src={img91} />
+            <Image classi="border-secondary border-1" src={img91} />
           </div>
           <div className="col-4 offset-1">
             <Image src={img9} />
@@ -256,6 +262,36 @@ export default function firstIssue() {
               appear next to #responsible.These hashtags were brought together with #farmers, indicating the link with agricultural activities. #responsible was also brought together with #mercosur, a trade bloc.
             </p>
 
+          </div>
+        </div>
+        <div className="row pt-5">
+          <div className="col-5 offset-1 mmb-n5">
+            <Heading3 classi="pb-3" text="Linking deforestation with contested agricultural practices" />
+            <p className="py-3">
+              Hashtags such as#gmo, #glyphosate, and #glifosato are associated  with #climatechange, #bolsonaro and #brazil. A company name, #monsanto and #boycott were brought together with #gmo and #glyphosate. In the case of #gmo, the following hashtags have been tweeted together: #deforestation, #plantatree, #forestfires, #bees, #agribusiness, #soybeans, #reforestation, and #protest.
+            </p>
+          </div>
+          <div className="col-10 offset-1" style={{ marginTop: '-30vh' }}>
+            <Image src={img10} />
+          </div>
+          <div className="col-5 offset-1">
+            <p>There was also a cluster referring to crime, misconduct and conflict,, including hashtags such as #warcrime, #corruption, #pollution, #tradewar, #landgrabbing, sitting closely with #leather, #europe, and #cop24. </p>
+            <p>These hashtags (#gmo, #glyphosate, and #glifosato) were positioned closely with #crimeagainsthumanity and #stopdeforestation. For instance, #crimeagainsthumanity was brought together with #ecocide, #GOP (the US Republican party), #environment and hashtags against Bolsonaro Salles.</p>
+          </div>
+        </div>
+
+        <div className="row pt-5">
+          <div className="col-5 offset-1 mmb-n5">
+            <Heading3 classi="pb-3" text="Linking the Amazon Fires with rights-related issues " />
+            <p>
+              {' '}
+              <span className="text-warning">Rights-related issues</span>
+              {' '}
+              have also been brought up, namely #animalrights, #landrightsnow, #indigenousirghts, #humanrights and #defendindigenousrights.
+            </p>
+          </div>
+          <div className="col-8 offset-2 mt-n5">
+            <Image src={img11} />
           </div>
         </div>
       </div>
