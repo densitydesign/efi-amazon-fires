64a52d09be6614d92030e7f8b0829a46cc276c5b
Federico <federico.izzop@gmail.com>
2021-10-20 15:08:09 +0200

    img5

--- src/pages/second-issue.js
+++ src/pages/second-issue.js
@@ -7,6 +7,11 @@ import Image from '../components/Image';
 import img1 from '../images/second-issue/1-intro.png';
 import img2 from '../images/second-issue/2-top.png';
 import img3 from '../images/second-issue/3-focus2.png';
+import img4 from '../images/second-issue/4-sentinel.png';
+import img41 from '../images/second-issue/4-nasa.png';
+import img42 from '../images/second-issue/4-science.png';
+import img43 from '../images/second-issue/4-united.png';
+import img5 from '../images/second-issue/5-tweets.png';
 
 export default function secondIssue() {
   return (
@@ -81,6 +86,69 @@ export default function secondIssue() {
           <div className="col-10 offset-1 pt-5">
             <Image src={img3} />
             <GraphQuote classi="py-5" emoticon="📉" text="Top hashtags overtime, resized by number of tweets (lower section)" />
+          </div>
+        </div>
+        <div className="row justify-content-between py-5">
+          <div className="col-3 offset-1">
+            <p>
+              {' '}
+              <span className="text-warning"> #sentinel2 (329 tweets)</span>
+              : the majority of tweets referred to this tweet by @CopernicusEU, the EU Earth Observation Programme (1).  It called for more attention on the fires in Africa which were happening simultaneously. Notably, #africafires emerged in the top hashtags in the peak period of the Amazon rainforest fires.
+            </p>
+            <Image src={img4} />
+          </div>
+          <div className="col-3 px-5">
+
+            <p>
+              <span className="text-light">#nasa (323 tweets)</span>
+              : Top tweets with this hashtag referred to NASA’s scientist Douglas Morton,  with his interview on Russia Today in Spanish (3). Another tweet was an endorsement for Douglas Morton by another academic (3).
+              {' '}
+            </p>
+            <Image src={img41} />
+          </div>
+          <div className="col-3">
+            <p>
+              <span className="text-indigo">#unitedbehindscience (215 tweets)</span>
+              : The majority of tweets quoted the tweet by @borisherrmann, a German offshore sailor and skipper of Team Malizia (a zero-carbon boat on which Greta Thunberg was sailing to New York at the time) (3). He tweeted a GIF of a live map with the hashtag #amazonfires. The hashtag #malizia also appeared on the top hashtag list.
+            </p>
+            <Image src={img43} />
+
+          </div>
+          <div className="col-1" />
+          <div className="col-3 offset-1 pt-4">
+            <p>
+              <span className="text-secondary">#sciencematters (217 tweets)</span>
+              : the majority of tweets with this hashtag promoted a tweet by @DrGJackBrown, a physician who has frequently appeared on CNN (2).
+              <p />
+            </p>
+            <Image src={img42} />
+          </div>
+        </div>
+
+        <div className="row pt-5">
+          <div className="col-4 offset-1">
+            <Heading3 classi="pb-3" text="Scientific voices emerge at the top only after the spike " />
+          </div>
+          <div className="col-6">
+            Scientific voices gathered visibility only after the spike when the number of tweets started to decrease in the peak period between 24 August to 2 September 2019. . As shown below, a few science-related tweets appeared at the top as the overall number of retweets slowed down. These tweets were from the users claiming to have a scientific or research background, namely @astro_luca (astronaut), @BiologistDan (biologist), and @ProfStrachan (energy policy).
+          </div>
+
+        </div>
+        <div className="row align-items-center">
+          <div className="col-7 offset-1">
+            <GraphQuote classi="py-5" emoticon="📉" text="Top 10 tweets per day for number of retweets" />
+            <Image src={img5} />
+          </div>
+          <div className="col-3">
+            <p>
+              A tweet by Luca Parmitano (@astro_luca), an Italian astronaut of the European Space Agency, emerged on the top retweet list on 29-30 August 2019, receiving more than 850 retweets. His tweet included aerial images of the Amazon fires.
+            </p>
+            <p>
+              A tweet by @ProfStrachan, the most active and influential account based on the number of tweets and mentions over the 10-day period, appeared at the top once the peak started to cease. One of his tweets referred to a Guardian article featuring the executive secretary of the UN convention on biological diversity.
+            </p>
+            <p>
+              Another tweet from a science-related account that appeared after the peak period was from @BiologistDan, an account claiming to be a biologist. The tweet points out the link between cattle farming and the fires.
+            </p>
           </div>
         </div>
       </div>
