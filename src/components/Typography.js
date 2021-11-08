1150807f512d70886ad441c47c0e3116e7bebc25
Federico <federico.izzop@gmail.com>
2021-11-08 16:14:42 +0100

    primi fix

--- src/components/Typography.js
+++ src/components/Typography.js
@@ -2,25 +2,25 @@ import '../scss/style.scss';
 
 export function Heading1({ text, classi }) {
   return (
-    <h1 className={`serif fw-bolder ${classi}`}>{text}</h1>
+    <h1 className={`serif    ${classi}`}>{text}</h1>
   );
 }
 
 export function Heading3({ text, classi }) {
   return (
-    <h3 className={`fw-bolder ${classi}`}>{text}</h3>
+    <h3 className={`   ${classi}`}>{text}</h3>
   );
 }
 
 export function Heading4({ text, classi }) {
   return (
-    <h4 className={`serif fw-bolder ${classi}`}>{text}</h4>
+    <h4 className={`serif    ${classi}`}>{text}</h4>
   );
 }
 
 export function Heading5({ text, classi }) {
   return (
-    <h5 className={`serif fw-bolder ${classi}`}>{text}</h5>
+    <h5 className={`serif    ${classi}`}>{text}</h5>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,8 +14,8 @@ export default function Index() {
         <div className="row align-items-center h-100">
           <div className="col-10 offset-1 position-relative">
             <img src={img1} className="img-fluid w-100" alt="..." />
-            <h1 className="position-absolute serif fw-bolder" style={{ bottom: '15%', left: '10%' }}>Out of the Flames</h1>
-            <h3 className="position-absolute serif fw-bolder" style={{ bottom: '10%', left: '10%' }}>Exploring online engagement around the 2019 Amazon rainforest fires</h3>
+            <h1 className="position-absolute serif  " style={{ bottom: '15%', left: '10%' }}>Out of the Flames</h1>
+            <h3 className="position-absolute serif  " style={{ bottom: '10%', left: '10%' }}>Exploring online engagement around the 2019 Amazon rainforest fires</h3>
           </div>
         </div>
       </div>
