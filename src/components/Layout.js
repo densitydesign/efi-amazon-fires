70ab0784c9b844ce491a9cfd26889a8ba0ce1b85
Federico <federico.izzop@gmail.com>
2021-12-21 22:29:49 +0100

    fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,7 +9,7 @@ export default function Layout({ children, section }) {
   return (
     <div>
       <Seo section={section} />
-      <div className="mobile text-white d-flex justify-content-center align-items-center">
+      <div className="mobile text-white d-flex d-lg-none d-md-none d-sm-none justify-content-center align-items-center">
         <div className="px-2">
           <p className="text-center">Oops</p>
           <p className="text-center">For a Better use of this website we recommend to use a pc or a tablet.</p>
