fa51309a8959d2f973b31c22219f125f1e444a0e
Federico <federico.izzop@gmail.com>
2022-01-17 17:52:32 +0100

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'efi-amazon-fires',
   },
-  pathPrefix: 'efi-amazon-fires',
+  // pathPrefix: 'efi-amazon-fires',
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
